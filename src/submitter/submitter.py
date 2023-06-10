1b719ef948c640c7e77ebd5982a139180ec0e55a
leonidee <leonide@de-deb-2.ru-central1.internal>
2023-06-10 12:02:13 +0000

    Refactored

--- src/submitter/submitter.py
+++ src/submitter/submitter.py
@@ -87,7 +87,7 @@ class SparkSubmitter(BaseRequestHandler):
 
         self._API_BASE_URL = os.getenv(_REQUIRED_VAR)
 
-    def submit_job(self, job: Literal["users_info_datamart_job", "location_zone_agg_datamart_job", "friend_recommendation_datamart_job"], keeper: ArgsKeeper) -> bool:  # type: ignore
+    def submit_job(self, job: Literal["collect_users_demographic_dm_job", "location_zone_agg_datamart_job", "friend_recommendation_datamart_job"], keeper: ArgsKeeper) -> bool:  # type: ignore
         """Sends request to API to submit Spark job in Hadoop Cluster.
 
         ## Parameters
@@ -141,10 +141,10 @@ class SparkSubmitter(BaseRequestHandler):
                 self.logger.debug(f"{response=}")
 
             except JSONDecodeError as e:
-                raise UnableToGetResponse(f"{e}. Posible failed to submit job.")
+                raise UnableToGetResponse(f"{str(e)}. Posible failed to submit job.")
 
-            if response.get("returncode") == 0:
-                self.logger.info(f"{job} job was submitted successfully!")
+            if response.get("returncode") == 2:
+                self.logger.info(f"'{job}' job was submitted successfully!")
 
                 self.logger.debug(f"Job stdout:\n{response.get('stdout')}")
                 self.logger.debug(f"Job stderr:\n{response.get('stderr')}")
@@ -159,7 +159,7 @@ class SparkSubmitter(BaseRequestHandler):
                 )
             else:
                 raise UnableToSubmitJob(
-                    f"Unable to submit '{job}' job. API returned code -> {response}"
+                    f"Unable to submit '{job}' job. API returned code -> {response.get('returncode')}"
                 )
         else:
             raise UnableToGetResponse(
--- tests/hand_test.py
+++ tests/hand_test.py
@@ -1,42 +1,28 @@
-import os
 import sys
-import time
-from dataclasses import dataclass
-from datetime import datetime, timedelta
-from enum import Enum
 from pathlib import Path
-from typing import Literal
 
-from requests.exceptions import JSONDecodeError
-
-sys.path.append(str(Path(__file__).parent.parent))
 # package
 sys.path.append(str(Path(__file__).parent.parent))
-from src.base import BaseRequestHandler
-from src.config import Config, EnableToGetConfig
-from src.environ import DotEnvError, EnvironNotSet
-from src.helper import S3ServiceError, SparkHelper
-from src.keeper import ArgsKeeper, SparkConfigKeeper
+from src.keeper import ArgsKeeper
 from src.logger import SparkLogger
-from src.spark import SparkRunner
+from src.submitter import SparkSubmitter
 
 logger = SparkLogger().get_logger(logger_name=__name__)
 
+spark = SparkSubmitter()
 
-from typing import Tuple
+def main() -> ...:
+    keeper = ArgsKeeper(
+        date="2022-04-26",
+        depth=10,
+        src_path="s3a://data-ice-lake-05/messager-data/analytics/geo-events",
+        tgt_path="s3a://data-ice-lake-05/messager-data/analytics/tmp",
+        coords_path="s3a://data-ice-lake-05/messager-data/analytics/cities-coordinates",
+        processed_dttm="2023-05-22T12:03:25",
+    )
+    spark.submit_job(job='collect_users_demographic_dm_job', keeper=keeper)
 
-
-def main(one: str, two: Tuple[str, str]) -> Tuple[str, ...]:
-    if one:
-        if two:
-            print("Hi!")
-    if not one:
-        raise ValueError("error!")
-    if not two:
-        raise KeyError("...")
-
-    return ()
 
 
 if __name__ == "__main__":
-    ...
+    main()
